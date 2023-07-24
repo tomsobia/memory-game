ce7af29d56a18a986f6ee074387b5b915be3f9fa
tomsobia <tom162sobia@gmail.com>
2023-07-24 21:17:00 +0300

    update

--- login.ts
+++ login.ts
@@ -1,23 +1,21 @@
 class User {
-    id:string
-    constructor(public username: string,) {
-        this.id = `id-${Math.random()}`;
-    }
+  id: string;
+  constructor(public username: string) {
+    this.id = `id-${Math.random()}`;
+  }
 }
 
 let Userarray: User[] = [];
-function handleAddUser(event){
-    
-    try {
-        event.preventDefault();
-        const username = event.target.username.value;
-        const user = new User(username);
-        Userarray.push(user);
-        localStorage.setItem(`user`, JSON.stringify(Userarray));
-        event.target.reset();
-        window.location.href = "index.html";
-
-    } catch (error) {
-        console.error(error);
-    }
+function handleAddUser(event) {
+  try {
+    event.preventDefault();
+    const username = event.target.username.value;
+    const user = new User(username);
+    Userarray.push(user);
+    localStorage.setItem(`user`, JSON.stringify(Userarray));
+    event.target.reset();
+    window.location.href = "index.html";
+  } catch (error) {
+    console.error(error);
+  }
 }
