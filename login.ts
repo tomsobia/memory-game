61d457bad16ada5e1c55ca9f64a656b9d7346e6e
ShaharShaul <128305593+ShaharShaul@users.noreply.github.com>
2023-07-26 14:12:14 +0300

    1

--- login.ts
+++ login.ts
@@ -1,72 +1,4 @@
 
-
-// class User {
-//   id: string;
-//   constructor(public username: string, public movesCount:number,public timeValue:number ) {
-//     this.id = `id-${Math.random()}`;
-//   }
-// }
-
-// let Userarray: User[] = [];
-
-// function handleAddUser(event: Event) {
-//   try {
-//     event.preventDefault();
-//     const target = event.target as HTMLFormElement;
-//     const username = target.username.value;
-//     const user = new User(username);
-//     Userarray.push(user);
-//     localStorage.setItem("users", JSON.stringify(Userarray));
-//     target.reset();
-//     window.location.href = "index.html";
-//   } catch (error) {
-//     console.error(error);
-//   }
-// }
-
-// document.getElementById("userForm")?.addEventListener("submit", handleAddUser);
-
-// function updateMovesCount(userId: string, movesCount: number) {
-//   const user = Userarray.find((u) => u.id === userId);
-//   if (user) {
-//     user.movesCount = movesCount;
-//   }
-// }
-
-
-// function updateTimeValue(userId: string, timeValue: number) {
-//   const user = Userarray.find((u) => u.id === userId);
-//   if (user) {
-//     user.timeValue = timeValue;
-//   }
-// }
-
-
-
-// function createResultsTable() {
-//   const resultsTableBody = document.getElementById("resultsTableBody");
-
-//   Userarray.forEach((user) => {
-//     const row = document.createElement("tr");
-//     const usernameCell = document.createElement("td");
-//     const movesCountCell = document.createElement("td");
-//     const timeValueCell = document.createElement("td");
-
-//     usernameCell.textContent = user.username;
-//     movesCountCell.textContent = user.movesCount.toString();
-//     timeValueCell.textContent = user.timeValue.toString();
-
-//     row.appendChild(usernameCell);
-//     row.appendChild(movesCountCell);
-//     row.appendChild(timeValueCell);
-
-//     resultsTableBody?.appendChild(row);
-//   });
-// }
-
-// // Call the function to populate the table when the page loads
-// window.onload = createResultsTable;
-
 class User {
     id: string;
     constructor(public username: string, public movesCount:number,public timeValue:number ) {
@@ -76,6 +8,13 @@ class User {
   
   let Userarray: User[] = [];
   
+  const users = localStorage.getItem('users');
+
+  const newArray: User[] = JSON.parse(users);
+  
+  if (newArray) {
+    Userarray = newArray;
+  } 
   function handleAddUser(event: Event) {
     try {
       event.preventDefault();
@@ -90,4 +29,4 @@ class User {
       console.error(error);
     }
   }
-  
+  console.log(newArray)
