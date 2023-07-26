80765fa8b146ff47a140aaa3b9a2799188164a6f
ShaharShaul <128305593+ShaharShaul@users.noreply.github.com>
2023-07-26 13:49:59 +0300

    last update

--- login.ts
+++ login.ts
@@ -67,4 +67,27 @@
 // // Call the function to populate the table when the page loads
 // window.onload = createResultsTable;
 
-
+class User {
+    id: string;
+    constructor(public username: string, public movesCount:number,public timeValue:number ) {
+      this.id = `id-${Math.random()}`;
+    }
+  }
+  
+  let Userarray: User[] = [];
+  
+  function handleAddUser(event: Event) {
+    try {
+      event.preventDefault();
+      const target = event.target as HTMLFormElement;
+      const username = target.username.value;
+      const user = new User(username);
+      Userarray.push(user);
+      localStorage.setItem("users", JSON.stringify(Userarray));
+      target.reset();
+      window.location.href = "index.html";
+    } catch (error) {
+      console.error(error);
+    }
+  }
+  
