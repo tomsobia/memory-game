b7491c23b642920480a2fc611ab3d70d82bd26fb
hadaritzhaki <127751975+hadaritzhaki@users.noreply.github.com>
2023-07-26 14:20:17 +0300

    ..

--- login.ts
+++ login.ts
@@ -1,28 +1,4 @@
-class User {
-  id: string;
-  constructor(public username: string, public movesCount:number,public timeValue:number ) {
-    this.id = `id-${Math.random()}`;
-  }
-}
 
-<<<<<<< HEAD
-let Userarray: User[] = [];
-
-function handleAddUser(event: Event) {
-  try {
-    event.preventDefault();
-    const target = event.target as HTMLFormElement;
-    const username = target.username.value;
-    const user = new User(username);
-    Userarray.push(user);
-    localStorage.setItem("users", JSON.stringify(Userarray));
-    target.reset();
-    window.location.href = "index.html";
-  } catch (error) {
-    console.error(error);
-  }
-}
-=======
 class User {
     id: string;
     constructor(public username: string, public movesCount:number,public timeValue:number ) {
